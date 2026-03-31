852ebbf211ff3566f75250430714b87f7b35c660
whoisthereitsme <destombesl@gmail.com>
2026-03-31 19:19:07 +0200

    committing changes

--- install.py
+++ install.py
@@ -1,7 +1,33 @@
 from pathlib import Path
+import subprocess
 import sys
 
-from xonnel_cmd import XCmd
+
+
+class XCmd:
+    MODES = {
+        "SILENT": dict(stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL),
+        "VERBOSE": dict(stdout=subprocess.PIPE, stderr=subprocess.PIPE),
+        "ERROR": dict(stdout=subprocess.DEVNULL, stderr=subprocess.PIPE),
+        "LIVE": dict(stdout=None, stderr=None)
+    }
+
+    @staticmethod
+    def exec(cmd:str="", cwd:str|Path=None, check:bool=True, shell:bool=True, mode="ERROR"):
+        mode: dict = XCmd.MODES.get(mode, XCmd.MODES["LIVE"])
+        stdout:int = mode.get("stdout", subprocess.PIPE)
+        stderr:int = mode.get("stderr", subprocess.PIPE)
+        return subprocess.run(cmd, shell=shell, check=check, cwd=cwd, stdout=stdout, stderr=stderr)
+
+    @staticmethod
+    def setcwd(cwd:str|Path=None, check:bool=True, shell:bool=True):
+        return subprocess.run(f"cd {cwd}", shell=shell, check=check)
+
+    @staticmethod
+    def getcwd():
+        return Path.cwd()
+    
+    
 from xonnel_git import XGit
 
 
