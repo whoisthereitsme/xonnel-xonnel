f4fe54b734e1e1ff1ea2fd59b6dc3c6ade996ffe
whoisthereitsme <destombesl@gmail.com>
2026-04-02 20:04:32 +0200

    committing changes

--- xonnel-xonnel/build/lib/build/lib/xonnel_xonnel/xxonnel/xxonnel.py
+++ xonnel-xonnel/build/lib/build/lib/xonnel_xonnel/xxonnel/xxonnel.py
@@ -1,192 +1,68 @@
-# [!] {+} IMPORTS
-from typing import TYPE_CHECKING
 
-# [|] {+} IMPORTS TYPING
-if TYPE_CHECKING:
-    ...
 
-# [|] {-}
 
 
 
 
 
-# [|] {+} IMPORTS 3RD PARTY
-...
 
-# [|] {-}
-
-
-
-
-
-# [|] {+} IMPORTS MYMODULES
-...
-
-# [|] {-}
-
-
-
-
-
-# [!] {-}
-
-
-
-
-
-
-
-# [!] {+} GLOBALS
-# [|] {+} GLOBAL CONSTANTS
-...
-
-# [|] {-}
-
-
-
-
-
-# [|] {+} GLOBAL VARIABLES
-...
-
-# [|] {-}
-
-
-
-
-
-# [|] {+} GLOBAL ALIASES
-...
-
-# [|] {-}
-
-
-
-
-
-# [|] {+} GLOBAL FUNCTIONS
-def global_function1():
-    ...
-    return ...
-
-# [|] {-}
-
-
-
-
-
-# [!] {-}
-
-
-
-
-
-
-
-# [!] {+} CLASSES
-class XXonnel:
-    # [|] {+} ATTRIBUTES
-    ...
-
-    # [|] {-}
-
-
-
-
-
-    # [|] {+} INITIALIZING METHODS
-    def __init__(self):
-        self.init()
-        self.post()
-        ...
-
-
-    def init(self):
-        ...
-
-    def post(self):
-        ...
-
-    # [|] {-}
-
-
-
-
-
-    # [|] {+} NORMAL METHODS
-    def method1(self):
-        ...
-        return ...
-
-    # [|] {-}
-
-
-
-
-
-    # [|] {+} PUBLIC METHODS
-    def getdata(self):
-        data = None
-        return data
-
-    # [|] {-}
-
-
-
-
-
-    # [|] {+} DUNDER METHODS
-    def __repr__(self):
-        return f"<{self.__class__.__name__}()>"
-
-    # [|] {-}
-
-
-
-
-
-# [!] {-}
-
-
-
-
-
-
-
-# [!] {+} SPECIALS
-def main():
-    ...
-    return ...
-
-def test():
-    ...
-    return ...
-
-# [!] {-}
-
-
-
-
-
-
-
-# [!] {+} EXECUTION
-if __name__ == "__main__":
-    main()
-
-# [!] {-}
-
-
-
-
-
-
-
-# [!] {+} DOCSPACE
-...
-
-# [!] {-}
-
-
-
+from xonnel_cmd                 import XCmd
+from xonnel_copy                import XCopy
+from xonnel_delete              import XDelete
+from xonnel_emoji               import XEmoji
+from xonnel_ensure              import XEnsure
+from xonnel_execute             import XExecute
+from xonnel_exit                import XExit
+from xonnel_file                import XFile
+from xonnel_git                 import XGit
+from xonnel_ip                  import XIp
+from xonnel_load                import XLoad
+from xonnel_move                import XMove
+from xonnel_pack                import XPack
+from xonnel_pad                 import XPad
+from xonnel_rand                import XRand
+from xonnel_save                import XSave
+from xonnel_stat                import XStat
+from xonnel_stats               import XStats
+from xonnel_stats_cpu           import XStatsCpu
+from xonnel_stats_disk          import XStatsDisk
+from xonnel_stats_gpu           import XStatsGpu
+from xonnel_stats_net           import XStatsNet
+from xonnel_stats_ram           import XStatsRam
+from xonnel_stats_vram          import XStatsVram
+from xonnel_stats_win           import XStatsWin
+from xonnel_unpack              import XUnpack
+from xonnel_www                 import XWww
+
+
+
+
+__all__ = [
+    "XCmd",
+    "XCopy",
+    "XDelete",
+    "XEmoji",
+    "XEnsure",
+    "XExecute",
+    "XExit",
+    "XFile",
+    "XGit",
+    "XIp",
+    "XLoad",
+    "XMove",
+    "XPack",
+    "XPad",
+    "XRand",
+    "XSave",
+    "XStat",
+    "XStats",
+    "XStatsCpu",
+    "XStatsDisk",
+    "XStatsGpu",
+    "XStatsNet",
+    "XStatsRam",
+    "XStatsVram",
+    "XStatsWin",
+    "XUnpack",
+    "XWww", 
+]
