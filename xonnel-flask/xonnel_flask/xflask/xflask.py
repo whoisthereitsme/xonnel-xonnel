919a11a5811eb08bf801cd6fe24322fee360ccdc
whoisthereitsme <destombesl@gmail.com>
2026-04-03 15:05:06 +0200

    committing changes

--- xonnel-flask/xonnel_flask/xflask/xflask.py
+++ xonnel-flask/xonnel_flask/xflask/xflask.py
@@ -6,12 +6,21 @@ if TYPE_CHECKING:
 
 
 import flask
+from xflaskview import XFlaskView
+
+
+
+
+
+
+
+
+
+
 
 
 
 class XFlask:
-    METHODS = ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"]
-
     def __init__(self):
         self.init()
         
@@ -19,12 +28,14 @@ class XFlask:
     def init(self):
         self.app = flask.Flask(__name__)
 
-    def _methods(self, methods:list):
-        for method in methods:
-            if method not in self.METHODS:
-                raise ValueError(f"Invalid HTTP method: {method}")
+    def _methods(self, view:XFlaskView=None):
+        return [method.upper() for method in ["get", "post", "put", "delete", "patch", "options", "head"] if hasattr(view, method)]
 
-    def rule(self, route:str="/", callback:callable=None, methods=["GET"]):
-        self._methods(methods)
-        self.app.add_url_rule(route, callback.__name__, callback, methods=methods)
+    def rule(self, route:str="/", view:XFlaskView=None):
+        methods = self._methods(view=view)
+        self.app.add_url_rule(route, view.__name__, view.as_view(view.__name__), methods=methods)
+
+
+
+
 
--- xonnel-flask/xonnel_flask/xflask/xflaskview.py
+++ xonnel-flask/xonnel_flask/xflask/xflaskview.py
@@ -2,7 +2,8 @@
 
 
 
-import base64
+
+
 
 
 from flask.views import MethodView
@@ -11,51 +12,91 @@ from datetime import datetime
 
 
 
+
+from xonnel_codex import XCodex
+
+
+
+
+
+
 class XFlaskView(MethodView):
-    def _encode(self, data:bytes):
-        return base64.b64encode(data).decode("utf-8")
-    
-    def _decode(self, data:str):
-        return base64.b64decode(data.encode("utf-8"))
-    
-    def _data(self, msg:str="", **kwargs):
+    def _response(self, msg:str="", **kwargs):
         resp = {}
         resp["message"] = msg
         resp["date"] = datetime.now().strftime("%Y-%m-%d")
         resp["time"] = datetime.now().strftime("%H:%M:%S")
 
         if kwargs.get("data", None) is not None:
-            resp["data"] = self._encode(kwargs["data"])
+            mode = kwargs.get("mode", "zlib base64")
+            resp["data"] = XCodex.encode(data=kwargs["data"], mode=mode)
+            resp["mode"] = mode
+            kwargs.pop("data", None)
+            kwargs.pop("mode", None)
         
         resp.update(kwargs)
 
         return jsonify(resp)
     
-    def _501(self, msg:str="", **kwargs):
-        return self._data(msg=msg, **kwargs), 501
+
     
-    def _200(self, msg:str="", **kwargs):
-        return self._data(msg=msg, **kwargs), 200
+    def _100(self, msg:str="", code:int=100, **kwargs):
+        return self._response(msg="[INFO] " + msg, **kwargs), code
+    
+    def _200(self, msg:str="", code:int=200, **kwargs):
+        return self._response(msg="[OK] " + msg, **kwargs), code
+    
+    def _300(self, msg:str="", code:int=300, **kwargs):
+        return self._response(msg="[REDIRECT] " + msg, **kwargs), code
+    
+    def _400(self, msg:str="", code:int=400, **kwargs):
+        return self._response(msg="[ERROR CLIENT] " + msg, **kwargs), code
+    
+    def _500(self, msg:str="", code:int=500, **kwargs):
+        return self._response(msg="[ERROR SERVER] " + msg, **kwargs), code
     
 
+
+    def response(self, code:int=500, msg:str="", **kwargs):
+        if code >= 100 and code < 200:
+            return self._100(msg=msg, code=code, **kwargs)
+        elif code >= 200 and code < 300:
+            return self._200(msg=msg, code=code, **kwargs)
+        elif code >= 300 and code < 400:
+            return self._300(msg=msg, code=code, **kwargs)
+        elif code >= 400 and code < 500:
+            return self._400(msg=msg, code=code, **kwargs)
+        elif code >= 500 and code < 600:
+            return self._500(msg=msg, code=code, **kwargs)
+        else:
+            return self._500(msg="Invalid status code", code=500, **kwargs)
+    
+
+
+    
+    
+    
+    
+
+    
     def get(self):
-        return self._501(msg="GET method not implemented")
+        return self.response(msg="GET method not implemented",          code=405)
     
     def post(self):
-        return self._501(msg="POST method not implemented")
+        return self.response(msg="POST method not implemented",         code=405)
     
     def put(self):
-        return self._501(msg="PUT method not implemented")
+        return self.response(msg="PUT method not implemented",          code=405)
     
     def delete(self):
-        return self._501(msg="DELETE method not implemented")
+        return self.response(msg="DELETE method not implemented",       code=405)
     
     def patch(self):
-        return self._501(msg="PATCH method not implemented")
+        return self.response(msg="PATCH method not implemented",        code=405)
     
     def options(self):
-        return self._501(msg="OPTIONS method not implemented")
+        return self.response(msg="OPTIONS method not implemented",      code=405)
     
     def head(self):
-        return self._501(msg="HEAD method not implemented")
+        return self.response(msg="HEAD method not implemented",         code=405)
 
