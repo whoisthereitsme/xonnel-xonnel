6f8ac0cd9e92cef21d98df74deabde873fc8a086
whoisthereitsme <destombesl@gmail.com>
2026-04-03 15:27:16 +0200

    committing changes

--- xonnel-flask/xonnel_flask/xflask/xflask.py
+++ xonnel-flask/xonnel_flask/xflask/xflask.py
@@ -11,7 +11,7 @@ import flask
 
 
 from .xflaskview import XFlaskView
-
+from pathlib import Path
 
 
 
@@ -21,12 +21,18 @@ class XFlask:
     View:XFlaskView = XFlaskView
     Show = flask.render_template
 
-    def __init__(self):
+    def __init__(self, path:str|Path=None):
+        self.path = Path(path)
         self.init()
         
 
     def init(self):
-        self.app = flask.Flask(__name__)
+        self.app = flask.Flask(
+            __name__,
+            root_path=          str(self.path),
+            template_folder=    str(self.path / "templates"),
+            static_folder=      str(self.path / "static")
+        )
 
     def run(self, host="127.0.0.1", port=5000, debug=True, *args, **kwargs):
         self.app.run(host=host, port=port, debug=debug, *args, **kwargs)
